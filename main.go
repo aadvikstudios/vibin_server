b6a1ea9648955ec0964ecf0ed4c5083f008d0b6f
Yash <aadvikstudios@gmail.com>
2025-01-30 23:17:27 +0530

    added s3 routes

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	routes.RegisterActionRoutes(r, actionService)
 	routes.RegisterChatRoutes(r, chatService)
 	routes.RegisterMatchRoutes(r, matchService)
+	routes.RegisterS3Routes(r)
 	log.Println("Routes registered.")
 
 	// Add CORS middleware
